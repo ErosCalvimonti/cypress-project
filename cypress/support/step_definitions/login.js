b196c242aa6610148818fc700e77a56fb4bebb6f
Eros Calvimonti <karielcode@gmail.com>
2024-09-18 17:24:10 -0300

    Adding changes to the project

--- cypress/support/step_definitions/login.js
+++ cypress/support/step_definitions/login.js
@@ -3,10 +3,12 @@ import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
     Given ("I navigate to vescra homepage", () => {
         cy.visit("/")
+        // Assertion
+        cy.title().should('eq','Home | ecommerce')
     })
 
     When ("I click on Iniciar Sesion button", () => {
-
+        
     })
 
     And ("I entered my information for Registration", () => {
